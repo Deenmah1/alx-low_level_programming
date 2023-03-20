e172f2341d6f853e7e26ba7576c32638125c26ad
Deenmah1 <jemimna084@gmail.com>
2023-03-20 04:32:14 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print a line of code of upper and lowercase
+ *
+ * Return: 0
+ */
+int main(void);
+{
+	char lowercase
+	char uppercase
+
+	for (lowercase 'a'; lowercas <= 'z'; lowercase++)
+	for (uppercase 'A'; uppercase <= 'Z'; uppercase++)
+		putchar(lowercase);
+	putchar(uppercase);
+	putchar('\n');
+	return (0);
+}
