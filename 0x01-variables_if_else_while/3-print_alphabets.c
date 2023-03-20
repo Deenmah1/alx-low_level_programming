d97a04d81d4c8e90f0c8499955b0d7fc7f85ef6e
Deenmah1 <jemimna084@gmail.com>
2023-03-20 04:41:42 -0700

    aphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,8 @@ int main(void)
 	char lowercase;
 	char uppercase;
 
-	for (lowercase 'a'; lowercase <= 'z'; lowercase++)
-	for (uppercase 'A'; uppercase <= 'Z'; uppercase++)
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 		putchar(lowercase);
 	putchar(uppercase);
 	putchar('\n');
