253b3e87de297a866657af38ab0721f4c7b49cbd
Deenmah1 <jemimna084@gmail.com>
2023-03-20 22:04:11 -0700

    betty

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print aphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		((lowercase == 'q' || lowercase == 'e') != 1)
+			putchar(lowercase);
+                        putchar('\n');
+	return (0);
+}
