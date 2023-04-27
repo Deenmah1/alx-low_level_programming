075b0cffe513b2d5588cc4760f551f9866a25027
Deenmah1 <jemimna084@gmail.com>
2023-04-27 16:55:05 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,9 +31,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	while (temp->next)
+	{
+		temp = temp->next;
+		temp->next = new;
+	}
 
-		 temp = temp->next;
-
-	 temp->next = new;
-	 return (new);
+	return (new);
 }
